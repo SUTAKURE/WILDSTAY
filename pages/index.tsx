da72c5e607c82498c7de114c4e120ba70e6e1fd0
SUTAKURE <udonnamerou@gmail.com>
2021-11-03 13:47:30 +0900

    add baseurl

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,7 +2,7 @@ import dynamic from 'next/dynamic';
 
 function App() {
   const Map = dynamic(
-    () => import('../components/map/map'),
+    () => import('components/map/map'),
     { ssr: false }, // This line is important. It's what prevents server-side render
   );
   return <Map />;
