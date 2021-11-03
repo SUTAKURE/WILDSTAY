72d4b9a46b7aab8820296d144431039c0dc34be0
SUTAKURE <udonnamerou@gmail.com>
2021-11-03 09:49:33 +0900

    display map

---
+++ components/map.tsx
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import { Map as MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+
+export default class SimpleExample extends Component {
+  render() {
+    return (
+      <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false}>
+        <TileLayer
+          attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+          url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
+        />
+        <Marker position={[51.505, -0.09]}>
+          <Popup>
+            A pretty CSS3 popup. <br /> Easily customizable.
+          </Popup>
+        </Marker>
+      </MapContainer>
+    );
+  }
+}
