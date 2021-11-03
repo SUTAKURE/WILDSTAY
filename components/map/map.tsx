040a047d9be6334d7a10e8d1374a49f53a8eac98
SUTAKURE <udonnamerou@gmail.com>
2021-11-03 10:23:54 +0900

    full display map

--- components/map/map.tsx
+++ components/map/map.tsx
@@ -1,15 +1,17 @@
 import React from 'react';
 import { Map as MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import useWindowDimensions from '../dimension/dimension';
 import 'leaflet/dist/leaflet.css';
 
 const Map = () => {
+  const { width, height } = useWindowDimensions();
   return (
     <>
       <MapContainer
         center={[51.505, -0.09]}
         zoom={13}
-        scrollWheelZoom={false}
-        style={{ height: 400, width: '100%' }}
+        scrollWheelZoom={true}
+        style={{ height: height, width: '100%' }}
       >
         <TileLayer
           attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
